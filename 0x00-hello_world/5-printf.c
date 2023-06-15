92b7c3c7e9dbbdb5e733ce527ca4dd1363af9f5d
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-06-15 04:48:47 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 # include <stdio.h>
 /*
- *
  * main - Entry POint
+ *
  * Return : always 0 (success)
  */
-int main (void)
+int main(void)
 {
+
+/*thie main block*/
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
