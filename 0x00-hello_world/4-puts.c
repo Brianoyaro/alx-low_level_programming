0a312ee3e5fd1cdbb6a0aae1ef7362fc92375866
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-06-15 04:34:58 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 # include <stdio.h>
-
+/*
+ * main - starting point
+ * return - prints 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
