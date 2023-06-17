7134e3f0020c7bec3d0902cf913e8273e8048d25
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-06-16 21:51:34 -0700

    all alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,8 +7,12 @@
  */
 int main(void)
 {
+	int a;
 	int A;
-	for (A = 0;A <= 25; A++)
+
+	for (a = 0;a <= 25; a++)
+		putchar('a' + a);
+	for (A = 0; A <= 25; A++)
 		putchar('A' + A);
 	putchar('\n');
 	return (0);
