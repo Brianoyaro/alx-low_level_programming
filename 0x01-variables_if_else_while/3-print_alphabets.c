d5fbf6772da12e14c0dfb0699385b8feadd5bf64
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-06-16 21:42:13 -0700

    upper alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int A;
+	for (A = 0;A <= 25; A++)
+		putchar('A' + A);
+	putchar('\n');
+	return (0);
+}
