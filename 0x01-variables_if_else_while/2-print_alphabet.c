6d75f485f159cfac3d819b7eed6be87dce292eef
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-06-16 21:28:06 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	for(a = 0; a <= 25; a++)
+		putchar('a' + a);
+	putchar('\n');
+	return (0);
+}
