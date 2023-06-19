22a22e9f416a3d0862856b65b7bae64c20d5ea43
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-06-19 07:25:31 -0700

    print combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Retrurn: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int a;
 
-	for (a = 0;a < 100;a++)
+	for (a = 0; a < 100; a++)
 	{
 		if (a < 100)
 		{
 			putchar((a / 10) + '0');
-			putchar((a % 10) + '0');			
+			putchar((a % 10) + '0');	
 			putchar(',');
 			putchar(' ');
 		}
