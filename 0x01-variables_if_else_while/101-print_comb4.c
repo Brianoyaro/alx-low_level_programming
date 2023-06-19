1c26a2fa21713ef5a8612dda38a8e0f1d2da140e
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-06-19 09:44:41 -0700

    print combo 4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a =0; a < 8; a++)
+	{
+		for (b =1; b < 9; b++)
+		{
+			for (c = 2; c < 9; c++)
+			{
+				if (a != b && a != c && b != c)
+				{
+					putchar('a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
+					if (a + b + c < 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
