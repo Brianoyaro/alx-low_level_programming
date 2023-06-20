e68716e8aa15981706cbd2c6a2e56e0a5c6cb98f
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-06-20 09:01:47 -0700

    is lower

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*int _putchar(int c;*/
+int _putchar(int c;
 #include <unistd.h>
-
+/*
 int _putchar(int c) {
     return write(1, &c, 1);
-}
+}*/
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
