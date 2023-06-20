bee0da37eb170a76a77e835a3c45aca1c77c500d
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-06-20 03:19:53 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
 /*void print_alphabet(void);*/
-void print_alphabet(void)
+/*void print_alphabet(void)
 {
 	int a;
 	for (a = 0; a < 26; a++)
@@ -13,7 +14,7 @@ void print_alphabet(void)
 		putchar('a' + a);
 	}
 	putchar('\n');
-}
+}*/
 int main(void)
 {
 	print_alphabet();
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,11 @@
-#include "main.h"
-
-
-/**
- * main - Check the code.
- *
- * Return: Always 0.
- */
-int main(void)
+#include <stdio.h>
+void print_alphabet(void);
+void print_alphabet(void)
 {
-	return (0);
+        int a;
+        for (a = 0; a < 26; a++)
+        {
+                putchar('a' + a);
+        }
+        putchar('\n');
 }
