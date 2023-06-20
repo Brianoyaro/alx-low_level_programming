cf24197fba52d9ce9d1b3673857a923abbbe9f3e
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-06-20 04:45:32 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *islower - checks for a lower case character.
+ *Return: Always 0.
+ */
+int _islower(int c)
+{
+	int a;
+
+	int islower(int c);
+	if ((int islower(int a)) == 1 )
+	{
+		a = 1;
+		printf("%d", a);
+	}
+	else
+	{
+		a = 0;
+		printf("%d", a);
+	}
+	return (a);
+}
