aa24532d17a8b868b77b20c81f8e95cf05f5242f
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-06-21 08:34:10 -0700

    104-fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 1;
+	int b = 2;
+	int sum;
+	int i;
+	printf("1, ");
+	for (i = 0; i < 48; i++)
+	{
+		sum = a + b;
+		a = b;
+		printf("%d", a);
+		printf(", ");
+		b = sum;
+		printf("%d", b);
+                printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
