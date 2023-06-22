e068f42515bf6dcd4ab7eb9e14b331e1283f94fa
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-06-22 04:30:30 -0700

    3-print_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,13 @@
 #include "main.h"
 /**
  * print_numbers - prints from 0 through 9
- *
  */
 void print_numbers(void)
 {
 	char i;
 
 	for (i = '0'; i < '9'; i++)
-	{
 		_putchar(i);
-	}
 
 	_putchar('\n');
 }
