7dec47d455dba7e31f5b7612e9df295bd037bbe3
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-06-22 07:23:01 -0700

    5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *
+ */
+void more_numbers(void)
+{
+	int i;
+	int counter = 0;
+
+	while (counter < 11)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i > 9)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+		counter++;
+	}
+}
