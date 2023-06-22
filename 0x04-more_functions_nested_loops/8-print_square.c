7292bd753c84fd37947439282f111782de9bb859
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-06-22 09:17:40 -0700

    8-print_square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *
+ *print_square - prints a square
+ */
+void print_square(int size)
+{
+	if (size > 0)
+	{
+		int i = 0;
+		int j;
+
+		while (i < size)
+		{
+			j = 0;
+			while ( j < size)
+			{
+				_putchar('#');
+				j++;
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
+	else
+		_putchar('\n');
+}
