1c97e061edcc08b3e8212bf893fbdb7156650bfb
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-06-22 07:48:51 -0700

    6-print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_line - draws straight line in terminal
+ *n: number of steps in line
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+}
