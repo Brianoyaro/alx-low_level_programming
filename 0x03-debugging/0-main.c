c7e1bcb49bbab2dd255acf8755a898ec0683b3bb
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-06-22 19:20:14 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - tests the function printing psitive or negative integers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
