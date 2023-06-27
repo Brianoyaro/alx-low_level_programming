da84fbb3912ab71488de74083991eeeaad8b5022
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-06-26 19:21:27 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,8 +14,9 @@ void print_rev(char *s)
 		i++;
 		++s;
 	}
+	--s;
 	/*iterate through string from last element whilst printing each *s */
-	for (j = i - 1; j >= 0; j--)
+	for (j = i ; j > 0; j--)
 	{
 		_putchar(*s);
 		--s;
