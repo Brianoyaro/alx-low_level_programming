8011e772025aaabc125fd4cba4c307bd2cabb89e
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-06-27 06:14:24 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * _strlen - prints the length of a string
+ * @str: string to find length
+ * Return: length of the string
+ */
+int _strlen(char *str)
+{
+	int length = 0;
+
+	while (*str != '\0')
+	{
+		length++;
+		++str;
+	}
+	return (length);
+}
+/**
+ * puts_half -  prints half of a string, followed by a new line
+ * @str: string to be manipulated
+ */
+void puts_half(char *str)
+{
+	int len = _strlen(str);
+	int a;
+	int starting_point;
+	
+	starting_point = len / 2;
+	for (a = starting_point; a < len; a++)
+	{
+		if (a >= starting_point)
+			_putchar(str[a]);
+		
+	}
+	_putchar('\n');
+}
