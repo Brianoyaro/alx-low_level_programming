73aa5a4390dff94bda780ffe62c3ece9a60505c4
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-06-27 06:43:06 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -24,13 +24,17 @@ void puts_half(char *str)
 	int len = _strlen(str);
 	int a;
 	int starting_point;
-	
-	starting_point = len / 2;
+
+	if (len % 2 != 0)
+		starting_point = ((len / 2) + 1);
+
+	else
+		starting_point = len / 2;
 	for (a = starting_point; a < len; a++)
 	{
 		if (a >= starting_point)
 			_putchar(str[a]);
-		
+
 	}
 	_putchar('\n');
 }
