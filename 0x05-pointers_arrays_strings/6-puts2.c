e181739af90fe1586190126607fc88530dd56af2
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-06-27 02:13:24 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * _strlen - function that finds the length of a string
+ * @str: string whose length is to be computed
+ * Return: returns lenght of the string
+ */
 int _strlen(char *str)
 {
 	int len = 0;
@@ -11,15 +16,20 @@ int _strlen(char *str)
 	return (len);
 }
 
+/**
+ * puts2 -  prints every other character of a string
+ * starting with the first character, followed by a new line.
+ * @str: string to be manipulated by puts2 function
+ */
 void puts2(char *str)
 {
 	/*first find length of the string*/
 	int i = _strlen(str);
 	int a;
-	
+
 	for (a = 0; a < i; a++)
 	{
-		if (*str % 2 == 0)
+		if (a % 2 == 0)
 			_putchar(*str);
 
 		str++;
