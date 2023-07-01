c7ffd1173cecfa4ae8ec2bc87692f16a6e091070
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-07-01 05:35:48 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @s: string to encode
+ * Return: encoded string
+ */
+char *rot13(char *s)
+{
+	int s1[53] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	int s2[53] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+	int a = 0;
+	int b;
+
+	while (s[a] != '\0')
+	{
+		b = 0;
+		while  (s1[b] != '\0' && s1[b] != s[a])
+			b++;
+
+		if (s[a] == s1[b])
+			s[a] = s2[b];
+		a++;
+	}
+	return (s);
+}
