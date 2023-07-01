aca01fe9b444b33731c77d647c46b0426a58cab9
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-07-01 07:33:56 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,34 +1,19 @@
 #include "main.h"
 void print_number(int n)
-/*{
-	int value;
-	int mul = 1;
-
-	while ((n / mul) > 1)
-		mul *= 10;
-
-	for (;mul > 1;)
-	{
-		value = n / mul;
-		_putchar(value + '0');
-		n = n - (value * mul);
-		mul /= 10;
-	}
-}*/
 {
 	unsigned int value, r_mul;
 	unsigned int mul = 1;
 
 	if (n == 0)
 		_putchar(0 + '0');
-	if (n < 0)
+	else
 	{
-		_putchar('-');
-		n = -n;
-	}
-	if (n > 0)
-	{
-		while ((n / mul) > 1)
+	       if (n < 0)
+		{
+			_putchar('-');
+			n = -n;
+		}
+		while ((n / mul) >= 1)
 			mul *= 10;
 
 		r_mul = mul / 10;
