bcf0a5c4d121ff45d80bc4817f534152b38cdd0a
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-07-03 09:39:39 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,10 +9,9 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int a;
+
 	for (a = 0; a < n; a++)
-	{
-		*dest = src[a];
-		++dest;
-	}
-	return dest;
+		dest[a] = src[a];
+
+	return (dest);
 }
