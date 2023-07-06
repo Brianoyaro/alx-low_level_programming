293f628b20ab85c9137003dce62f944241d318be
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-07-05 19:49:21 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,20 +3,22 @@ int func_sqrt(int a, int b, int n);
 /**
  * _sqrt_recursion - finds natural square root of a number
  * @n: number to find square root
- * Return: square root of n 
+ * Return: square root of n
  * -1 if n lacks a natural square root
  */
 int _sqrt_recursion(int n)
 {
 	if (n == 1)
 		return (1);
-	return (func_sqrt(1,1,n));
+	return (func_sqrt(1, 1, n));
 }
 /**
  * func_sqrt - finds square root of n
  * @a: starting lower bound 1
  * @b: starting lower bound 2
  * @n: number to find square root
+ * Return: square root of n
+ * -1 if n lacks a natural square root
  */
 int func_sqrt(int a, int b, int n)
 {
@@ -26,5 +28,5 @@ int func_sqrt(int a, int b, int n)
 		return (a);
 	else if ((a * b) != n)
 		a++, b++;
-	return(func_sqrt(a, b, n));
+	return (func_sqrt(a, b, n));
 }
