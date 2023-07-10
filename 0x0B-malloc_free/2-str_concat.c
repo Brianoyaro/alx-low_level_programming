dc492b6ada4f5ae8dca8852c7f7cba8d4dc601c6
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-07-10 03:32:27 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,7 +18,7 @@ char *str_concat(char *s1, char *s2)
 		s2[0] = '\0';
 	length1 = length_finder(s1);
 	length2 = length_finder(s2);
-	value = malloc(sizeof(char) * (length1 + length2 - 1));
+	value = malloc(sizeof(char) * (length1 + length2 + 1));
 	if (value == NULL)
 		return (NULL);
 	for (i = 0; i < length1; i++)
