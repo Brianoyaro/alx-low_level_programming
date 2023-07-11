660f1123480882d204a9f0ac61a7aa8e2acb64e0
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-07-11 01:56:39 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,20 +16,23 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	arr = malloc(sizeof(int *) * height);
 	if (arr == NULL)
+	{
+		free(arr);
 		return (NULL);
+	}
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
+		{
+			free(arr[i]);
 			return (NULL);
+		}
 	}
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
 			arr[i][j] = 0;
 	}
-	for (i = 0; i < height; i++)
-		free(arr[i]);
-	free(arr);
 	return (arr);
 }
