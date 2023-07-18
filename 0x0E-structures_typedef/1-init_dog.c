e7f205d23bea7f06d26d9684e182f7b935068b3f
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-07-17 23:24:43 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,6 +8,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	/*check if pointer d was initialised well*/
+	if (d == NULL)
+		return;
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
