5cd0508492f85fb46c709cb1696599f3ff8b8fca
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-07-19 11:16:01 -0700

    3-calc.h 3-get_op_func.c 3-main.c 3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,50 @@
+/**
+ * op_add - adds two numbers
+ * @a: firsat number
+ * @b: second number
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts two numbers
+ * @a: first number
+ * @b: second number
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides two numbers
+ * @a: first number
+ * @b: second number
+ * Return: division of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - does modulus of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: modulo of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
