549c502f3ce43a89cb8c30a575f1afd3b76e38ef
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-07-27 19:18:53 -0700

    fixed len bug in 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,6 +2,22 @@
 #include <string.h>
 #include <stdlib.h>
 /**
+ * _strlen - finds length of a string
+ * @str: string to find length
+ * Return: length of str
+ */
+int _strlen(const char *str)
+{
+	int len = 0;
+
+	while (*str)
+	{
+		++len;
+		++str;
+	}
+	return (len);
+}
+/**
  * add_node - adds a node at the beginning
  * @head: first node of the list
  * @str: string to copy to sttring part of node created
@@ -15,6 +31,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->str = strdup(str);
+	new_node->len = _strlen(str);
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
