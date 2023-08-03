1d88c086adb949dd2333bbaf92b78d229038b01a
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-08-03 10:37:59 -0700

    3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * set_bit - sets bit at specified index to 1
+ * @n: number to set bit
+ * @index: position of number to change
+ * Return: 1 on success
+ * else -1 on error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	int mask;
+	unsigned long int size;
+
+	size = sizeof(long int)*8;
+	if (index > size)
+		return (-1);
+	mask = 1 << index;
+	*n = ((*n & ~mask) | (1 << index));
+	return (1);
+}
