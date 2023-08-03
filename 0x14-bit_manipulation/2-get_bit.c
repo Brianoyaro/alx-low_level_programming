99b6d8cc8347aaebb7bcda3d1712a3c25da928fc
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-08-03 10:25:07 -0700

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * get_bit - gets bit type at given index
+ * @n: number to find byte from
+ * @index: specific position to obtain bit
+ * Return: bit type at specified index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int lenn;
+
+	/*lenn = sizeof(unsigned int) * 8;*/
+	lenn = 4294967295;
+	if (index > lenn)
+		return (-1);
+	return (1 & (n >> index));
+}
