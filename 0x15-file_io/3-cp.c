b1df010cf6555bad9d293d4ca61417c38a822347
Brianoyaro <Brianoyaro2000@yahoo.com>
2023-08-07 03:57:00 -0700

    3-op.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,26 +17,26 @@ int main (int argc, char **argv)
 		exit(97);
 	}
 	fd1 = open(argv[1], O_RDONLY);
-	if (fd1 < 0)
+	if (fd1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fd2 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-	if (fd2 < 0)
+	if (fd2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	while ((n_read = (read(fd1, buff, 1024))) != 0)
 	{
-		if (n_read < 0)
+		if (n_read == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
 			exit(98);
 		}
 		j = write(fd2, buff, n_read);
-		if (j < 0)
+		if (j == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
